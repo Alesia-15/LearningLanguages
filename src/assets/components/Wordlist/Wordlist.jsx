acefb912c1b3fc23e5bdf91afaa990b80e272503
Alesia-15 <alice15sh@gmail.com>
2023-09-15 00:47:31 +0300

    getInput

--- src/assets/components/Wordlist/Wordlist.jsx
+++ src/assets/components/Wordlist/Wordlist.jsx
@@ -6,23 +6,36 @@ import del from "../../images/delete.svg";
 import close from "../../images/close.svg";
 
 function Wordlist(props) {
-  const [passed, setPassed] = useState(props.passed || false);
-  const handleClick = () => {
-    setPassed(!passed);
+  // открытие / закрытие режима редактирования
+  const [pressed, setPressed] = useState(props.pressed || false);
+  const handleClickOpenClose = () => {
+    setPressed(!pressed);
   };
+
+  // input с проверкой на пустоту
+  function getInput(el) {
+    return (
+      <input
+        type="text"
+        defaultValue={el}
+        className={`${el === "" ? "empty" : ""}`}
+      />
+    );
+  }
+
   return (
     <>
-      {passed ? (
+      {pressed ? (
         <div className="listContainer">
-          <input type="text" defaultValue={props.english} />
-          <input type="text" defaultValue={props.transcription} />
-          <input type="text" defaultValue={props.russian} />
-          <input type="text" defaultValue={props.topic} />
+          {getInput(props.english)}
+          {getInput(props.transcription)}
+          {getInput(props.russian)}
+          {getInput(props.topic)}
           <div className="btn">
             <button>
               <img src={save} alt="save" />
             </button>
-            <button onClick={handleClick}>
+            <button onClick={handleClickOpenClose}>
               <img src={close} alt="close"></img>
             </button>
           </div>
@@ -34,7 +47,7 @@ function Wordlist(props) {
           <p>{props.russian}</p>
           <p>{props.topic}</p>
           <div>
-            <button onClick={handleClick}>
+            <button onClick={handleClickOpenClose}>
               <img src={pen} alt="pen"></img>
             </button>
             <button>
